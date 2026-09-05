bf8cc5c924f7e54704414d0b5abd077941efd36c
Adeyemi Anuoluwapo <adeyemianuoluwapo97@gmail.com>
2026-09-05 20:45:08 +0100

    Convert userModel.js to ES6 module syntax

--- model/userModel.js
+++ model/userModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -16,4 +16,4 @@ const userSchema = new mongoose.Schema({
 });
 
 const userModel= mongoose.model('User', userSchema);
-module.exports = userModel;
+export default userModel;
